143bc166690789114a0a94db692f94be873bd608
ytturi <ytturi@protonmail.com>
2018-03-03 12:49:43 +0100

    Add getServerVersion logic

--- api/sender.py
+++ api/sender.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
+import requests
 
 
 logger = logging.getLogger('RequestSender')
@@ -7,6 +8,7 @@ logger = logging.getLogger('RequestSender')
 ORION_SERVER_PRIVATE = '192.168.4.230'
 ORION_SERVER_PUBLIC = '84.89.60.4'
 ORION_PORT = 80
+BASE_URL = 'http://{server}:{port}/'
 
 
 def registerEntity():
@@ -14,6 +16,16 @@ def registerEntity():
     return True
 
 
-def getServerVersion():
+def getServerVersion(private=False):
     logger.info('Requesting Server Version')
-    return True
+    server = ORION_SERVER_PRIVATE if private else ORION_SERVER_PUBLIC
+    url = BASE_URL.format(server=server, port=ORION_PORT)
+    response = requests.get(url)
+    if response.status_code != 200:
+        logger.error('Failed to Get Server Version!')
+        logger.error(str(response.content))
+        return response.status_code
+    data = response.content['orion']
+    logger.info('Version: {}'.format(data['version']))
+    logger.info('Uptime: {}'.format(data['uptime']))
+    return response.status_code
