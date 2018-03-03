1d03e4b83756b1e9c4e4e99ea65f80f5e6d8b86f
ytturi <ytturi@protonmail.com>
2018-03-03 14:58:47 +0100

    Add getServerVersion requirement before listening

--- listener.py
+++ listener.py
@@ -17,16 +17,21 @@ def default():
 
 
 @click.command()
-@click.option('-v', '--verbosity', help='Show verbose output')
-@click.option('-p', '--port', type=int, help='Port to listen on')
-@click.option('-i', '--interface', type=str, help='Interface to bind')
+@click.option('-v', '--verbosity', is_flag=True, help='Show verbose output')
+@click.option('-p', '--port', default=80, type=int, help='Port to listen on')
+@click.option(
+    '-i', '--interface', default='0.0.0.0', type=str, help='Interface to bind')
 def startup(verbosity, port, interface):
-    logging.basicConfig(format='[%(asctime)s]%(name)s: %(message)s',
-                        datefmt='%Y/%m/%d-%H:%M:%S',
-                        level=logging.INFO if verbosity else logging.WARNING)
+    logging.basicConfig(
+        format='[%(asctime)s][%(levelname)s]%(name)s: %(message)s',
+        datefmt='%Y/%m/%d-%H:%M:%S',
+        level=logging.INFO if verbosity else logging.WARNING
+    )
     global house_core
     house_core = Core()
-    sender.registerEntity()
+    if sender.getServerVersion() != 200:
+        exit(-1)
+    # sender.registerEntity()
     app.run(host=interface, port=port)
 
 
