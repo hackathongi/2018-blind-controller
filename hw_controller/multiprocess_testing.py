50522e5c728e9128d94175f513e598d721112c07
AdriNicu <u1915748@campus.udg.edu>
2018-03-03 15:28:25 +0100

    Testing multiprocess calls.

---
+++ hw_controller/multiprocess_testing.py
@@ -0,0 +1,29 @@
+import click
+import multiprocessing import Pool
+import hw_controller
+
+
+
+@click.command()
+def testing():
+    print("Start of the execution!")
+    pool = Pool(processes=1)
+    try:
+        while True:
+            action = raw_input('So?: ')
+            if action == "open":
+                pool.close()
+                pool = Pool(processes=1)
+                pool.apply_async(hw_controller.blind_open())
+            elif action == "close":
+                pool.close()
+                pool = Pool(processes=1)
+                pool.apply_async(hw_controller.blind_open())
+            else:
+                print(action+" is not a valid option...")
+    except KeyboardInterrupt:
+        print("End of the execution...")
+
+
+if __name__ == '__main__':
+    testing()
