f765bb84980b698fcbb4cbfe04753d0d2f541b92
polsala <polsalalidon@gmail.com>
2018-03-03 15:57:28 +0100

    Check point.

--- core/core.py
+++ core/core.py
@@ -1,8 +1,28 @@
 # -*- encoding: utf-8 -*-
 from elements import ElementManager
 
-
 class Core(object):
 
+    MIN_LUX = 400
+    MIN_TMP = 20
+
+    MAX_LUX = 200
+    MAX_TMP = 30
+
     def __init__(self):
-        self.manager = ElementManager()
+        self.temperature_lvl = 0
+        self.light_lvl = 0
+        #self.manager = ElementManager()
+
+
+
+    def action_selector(self):
+        '''call determinate function depending of environement status'''
+        current_temp = self.temperature_lvl
+        current_light = self.light_lvl
+
+        # Check Extream vals
+        if current_light in range(self.MIN_LUX, self.MAX_LUX):
+
+
+    def write(self):
