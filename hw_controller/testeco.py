9d06943102bf1e0837e3cdfcbd8eac9f9e0f823c
AdriNicu <u1915748@campus.udg.edu>
2018-03-03 20:10:06 +0100

    baijsdlfjkas.

--- hw_controller/testeco.py
+++ hw_controller/testeco.py
@@ -15,7 +15,7 @@ def testeco():
             GPIO.output(13, GPIO.HIGH)
             time.sleep(1)
             GPIO.output(13, GPIO.LOW)
-            i = GPIO.input(13)
+            i = GPIO.input(15)
             print("Val: "+str(i))
             pass
     except KeyboardInterrupt:
