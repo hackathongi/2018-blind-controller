8479e5efcb2a86cb6621c4c2a18a75be4a8705ff
AdriNicu <u1915748@campus.udg.edu>
2018-03-03 16:08:49 +0100

    Add an stop option to finish the execution.

--- hw_controller/multiprocess_testing.py
+++ hw_controller/multiprocess_testing.py
@@ -7,36 +7,40 @@ import time
 
 @click.command()
 def testing():
+
     print("Start of the execution!")
+    stop = 0
     pool = Pool(processes=1)
-    try:
-        while True:
-            action = raw_input('Action?: ')
-            if action == "open":
-                t = raw_input('Time: ')
-                if t == '':
-                    t = '0'
-                pool.close()
-                pool.terminate()
-                pool = Pool(processes=1)
-                pool.apply_async(hw_controller.blind_open, (int(t),))
-            elif action == "close":
-                t = raw_input('Time: ')
-                if t == '':
-                    t = '0'
-                pool.close()
-                pool.terminate()
-                pool = Pool(processes=1)
-                pool.apply_async(hw_controller.blind_close, (int(t),))
-            elif action == "purge":
-                pool.close()
-                pool.terminate()
-                pool = Pool(processes=1)
-            else:
-                print(action+" is not a valid option...")
-            time.sleep(0.5)
-    except KeyboardInterrupt:
-        print("End of the execution...")
+
+    while not stop:
+        action = raw_input('Action?: ')
+        if action == "open":
+            t = raw_input('Time: ')
+            if t == '':
+                t = '0'
+            pool.close()
+            pool.terminate()
+            pool = Pool(processes=1)
+            pool.apply_async(hw_controller.blind_open, (int(t),))
+        elif action == "close":
+            t = raw_input('Time: ')
+            if t == '':
+                t = '0'
+            pool.close()
+            pool.terminate()
+            pool = Pool(processes=1)
+            pool.apply_async(hw_controller.blind_close, (int(t),))
+        elif action == "purge":
+            pool.close()
+            pool.terminate()
+            pool = Pool(processes=1)
+        elif action == "stop":
+            stop = 1
+        else:
+            print(action+" is not a valid option...")
+        time.sleep(0.5)
+        
+    print("End of the execution...")
 
 
 if __name__ == '__main__':
